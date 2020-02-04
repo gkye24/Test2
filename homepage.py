aeda57b6a258e79ffc12ec4d8541811e7055ec19
gkye24 <geenak1020601@gmail.com>
2020-02-04 08:54:44 -0600

    test

--- homepage.py
+++ homepage.py
@@ -1,17 +1,17 @@
 import tkinter
 import pygame
 
-#background
+# background
 pygame.init()
 
-window_width=500
-window_height=300
+window_width = 500
+window_height = 300
 
-animation_increment=10
+animation_increment = 10
 clock_rate = 20
 
-size=(window_width, window_height)
-screen=pygame.display.set_mode(size)
+size = (window_width, window_height)
+screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Homepage")
 
@@ -20,7 +20,7 @@ dead = False
 clock = pygame.time.Clock()
 background_image = pygame.image.load("background1.gif").convert()
 
-while(dead==False):
+while (dead == False):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             dead = True
@@ -30,4 +30,18 @@ while(dead==False):
     pygame.display.flip()
     clock.tick(clock_rate)
 
-hi
+menu = True
+
+start_button = pygame.draw.rect(screen, (0, 0, 240), (150, 90, 100, 50))
+continue_button = pygame.draw.rect(screen, (0, 244, 0), (150, 160, 100, 50))
+quit_button = pygame.draw.rect(screen, (244, 0, 0), (150, 230, 100, 50))
+
+pygame.display.flip()
+
+while menu:
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pos() >= (150, 230):
+                if pygame.mouse.get_pos() <= (250, 280):
+                    pygame.quit()
