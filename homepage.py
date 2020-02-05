346691287ecaea5d55097ce75b93561667ec0b3e
gkye24 <geenak1020601@gmail.com>
2020-02-05 08:48:45 -0600

    test

--- homepage.py
+++ homepage.py
@@ -20,6 +20,26 @@ dead = False
 clock = pygame.time.Clock()
 background_image = pygame.image.load("background1.gif").convert()
 
+menu = True
+
+
+
+while menu:
+    screen.blit(background_image, [0, 0])
+    start_button = pygame.draw.rect(screen, (0, 0, 240), (150, 90, 100, 50))
+    continue_button = pygame.draw.rect(screen, (0, 244, 0), (150, 160, 100, 50))
+    quit_button = pygame.draw.rect(screen, (244, 0, 0), (150, 230, 100, 50))
+
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pos() >= (150, 230):
+                if pygame.mouse.get_pos() <= (250, 280):
+                    pygame.quit()
+
+    pygame.display.flip()
+    clock.tick(clock_rate)
+
 while (dead == False):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -29,19 +49,3 @@ while (dead == False):
 
     pygame.display.flip()
     clock.tick(clock_rate)
-
-menu = True
-
-start_button = pygame.draw.rect(screen, (0, 0, 240), (150, 90, 100, 50))
-continue_button = pygame.draw.rect(screen, (0, 244, 0), (150, 160, 100, 50))
-quit_button = pygame.draw.rect(screen, (244, 0, 0), (150, 230, 100, 50))
-
-pygame.display.flip()
-
-while menu:
-    for event in pygame.event.get():
-        print(event)
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if pygame.mouse.get_pos() >= (150, 230):
-                if pygame.mouse.get_pos() <= (250, 280):
-                    pygame.quit()
---
+++ jumping.py
@@ -0,0 +1,33 @@
+import tkinter
+import pygame
+
+# background
+pygame.init()
+
+window_width = 500
+window_height = 300
+
+animation_increment = 10
+clock_rate = 20
+
+size = (window_width, window_height)
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("Homepage")
+
+dead = False
+
+clock = pygame.time.Clock()
+background_image = pygame.image.load("ground1.jpg").convert()
+
+while (dead == False):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            dead = True
+
+    screen.blit(background_image, [0, 0])
+
+    pygame.display.flip()
+    clock.tick(clock_rate)
+
+menu = True
