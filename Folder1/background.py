e1a9245969013a8eea903f36f61245c70e0cfd63
natalieekim <nataliejihyunkim@gmail.com>
2020-02-19 08:16:27 -0500

    test

--- Folder1/background.py
+++ Folder1/background.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 import os
+import random
 
 pygame.mixer.init(44100, 16, 2, 4096)
 pygame.init()
@@ -10,23 +11,33 @@ pygame.mixer.music.load("happymusic.mp3")
 pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.play(-1)
 
+W, H = 500, 300
+win = pygame.display.set_mode((W, H))
+
+
 class player(object):
-    run = [pygame.image.load(os.path.join('duckresized.png'))]
-    jump = [pygame.image.load(os.path.join('duckresized.png'))]
+    run = pygame.image.load(os.path.join('duckresized.png'))
+    jump = pygame.image.load(os.path.join('duckresized.png'))
     jumpList = [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4,
                 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, -1, -1, -1, -1, -2, -2, -2, -2, -2, -2, -2, -2, -2,
                 -2, -2, -2, -3, -3, -3, -3, -3, -3,
                 -3, -3, -3, -3, -3, -3, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4]
-
-    def __init__(self, x, y):
+    fall = pygame.image.load(os.path.join('duckresized.png'))
+    def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
+        self.width = width
+        self.height = height
         self.jumping = False
+        self.falling = False
         self.jumpCount = 0
         self.runCount = 0
 
     def draw(self, win):
+        if self.falling:
+            win.blit(self.fall, (self.x, self.y + 30))
+
         if self.jumping:
             self.y -= self.jumpList[self.jumpCount] * 1.2
             win.blit(self.jump[self.jumpCount // 18], (self.x, self.y))
@@ -35,12 +46,48 @@ class player(object):
                 self.jumpCount = 0
                 self.jumping = False
                 self.runCount = 0
+            self.hitbox = (self.x+ 4,self.y,self.width-24,self.height-10)
 
         else:
             if self.runCount > 42:
                 self.runCount = 0
             win.blit(self.run[self.runCount // 6], (self.x, self.y))
             self.runCount += 1
+            self.hitbox = (self.x+ 4,self.y,self.width-24,self.height-13)
+
+        pygame.draw.rect(win, (255,0,0),self.hitbox, 2) # Draws hitbox
+
+
+class saw(object):
+    rotate = pygame.image.load(os.path.join('log.jpg'))
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.rotateCount = 0
+        self.vel = 1.4
+
+    def draw(self, win):
+        self.hitbox = (
+        self.x + 10, self.y + 5, self.width - 20, self.height - 5)  # Defines the accurate hitbox for our character
+        pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
+        if self.rotateCount >= 8:  # This is what will allow us to animate the saw
+            self.rotateCount = 0
+        win.blit(pygame.transform.scale(self.rotate[self.rotateCount // 2], (64, 64)),
+                 (self.x, self.y))  # scales our image down to 64x64 before drawing
+        self.rotateCount += 1
+
+    def collide(self, rect):
+        if rect[0] + rect[2] > self.hitbox[0] and rect[0] < self.hitbox[0] + self.hitbox[2]:
+            if rect[1] + rect[3] > self.hitbox[1]:
+                return True
+        return False
+
+obstacles = []
+
+pygame.time.set_timer(USEREVENT+2, random.randrange(2000, 3500)) # Will trigger every 2 - 3.5 seconds
 
 
 class Background:
@@ -53,15 +100,8 @@ class Background:
         self.clock_rate = clock_rate
         self.size = (window_width, window_height)
         self.screen = pygame.display.set_mode(self.size)
-        W, H = 500, 300
-        self.win = pygame.display.set_mode((W, H))
         self.bgX = 0
-        self.bgX2 = self.window_width
-
-    def redrawWindow(self):
-        self.win.blit(self.racetrackbg(), (self.bgX, 0))  # draws our first bg image
-        self.win.blit(self.racetrackbg(), (self.bgX2, 0))  # draws the second bg image
-        pygame.display.update()  # updates the screen
+        self.bgX2 = window_width
 
     def homepagebg(self):
         pygame.display.set_caption("Homepage")
@@ -176,6 +216,11 @@ class Background:
 
                 if event.type == pygame.QUIT:
                     menu = False
+
+                if event.type == USEREVENT + 2:
+                    r = random.randrange(0, 2)
+                    if r == 0:
+                        obstacles.append(saw(810, 310, 64, 64))
 
             pygame.display.update()
             pygame.display.flip()
@@ -338,3 +383,13 @@ class Background:
 
             pygame.display.flip()
             self.clock.tick(self.clock_rate)
+
+def redrawWindow():
+    win.blit(bg, (bgX, 0))  # draws our first bg image
+    win.blit(bg, (bgX2, 0))  # draws the second bg image
+    for obstacle in obstacles:
+        if obstacle.collide(runner.hitbox):
+            runner.falling = True
+        obstacle.draw(win)
+
+    pygame.display.update()  # updates the screen
