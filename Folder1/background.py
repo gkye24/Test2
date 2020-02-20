c657f1b9b0205dd88c7c7d0fdd323d0cd9aade3f
natalieekim <nataliejihyunkim@gmail.com>
2020-02-20 18:53:41 -0500

    test

--- Folder1/background.py
+++ Folder1/background.py
@@ -23,7 +23,6 @@ class player(object):
                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, -1, -1, -1, -1, -2, -2, -2, -2, -2, -2, -2, -2, -2,
                 -2, -2, -2, -3, -3, -3, -3, -3, -3,
                 -3, -3, -3, -3, -3, -3, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4]
-    fall = pygame.image.load(os.path.join('duckresized.png'))
 
     def __init__(self, x, y, width, height):
         self.x = x
@@ -35,12 +34,13 @@ class player(object):
         self.jumpCount = 0
         self.runCount = 0
 
+
     def draw(self, win):
-        if self.falling:
-            win.blit(self.fall, (self.x, self.y + 30))
+        # if self.falling:
+        #     win.blit(self.fall)
 
         if self.jumping:
-            self.y -= self.jumpList[self.jumpCount] * 1.2
+            self.y -= self.jumpList[self.jumpCount] * 3
             win.blit(self.jump, (self.x, self.y))
             self.jumpCount += 1
             if self.jumpCount > 108:
@@ -58,9 +58,8 @@ class player(object):
 
         # pygame.draw.rect(win, (255,0,0),self.hitbox, 2) # Draws hitbox
 
-
 class saw(object):
-    rotate = pygame.image.load(os.path.join('log.jpg'))
+    img1 = pygame.image.load(os.path.join('log.jpg'))
 
     def __init__(self, x, y, width, height):
         self.x = x
@@ -69,16 +68,12 @@ class saw(object):
         self.height = height
         self.rotateCount = 0
         self.vel = 1.4
+        self.hitbox = (self.x + 10, self.y + 5, self.width - 20, self.height - 5)
 
     def draw(self, win):
-        self.hitbox = (
-        self.x + 10, self.y + 5, self.width - 20, self.height - 5)  # Defines the accurate hitbox for our character
-        pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
-        if self.rotateCount >= 8:  # This is what will allow us to animate the saw
-            self.rotateCount = 0
-        win.blit(pygame.transform.scale(self.rotate[self.rotateCount // 2], (64, 64)),
-                 (self.x, self.y))  # scales our image down to 64x64 before drawing
-        self.rotateCount += 1
+        self.hitbox = (self.x + 10, self.y + 5, self.width - 20, self.height - 5)  # Defines the accurate hitbox for our character
+        # pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
+        win.blit(pygame.transform.scale(self.img1, (64, 64)), (self.x, self.y))  # scales our image down to 64x64 before drawing
 
     def collide(self, rect):
         if rect[0] + rect[2] > self.hitbox[0] and rect[0] < self.hitbox[0] + self.hitbox[2]:
@@ -86,11 +81,22 @@ class saw(object):
                 return True
         return False
 
+class spike(saw):
+    img2 = pygame.image.load(os.path.join('gem.jpg'))
 
-obstacles = []
+    def draw(self, win):
+        self.hitbox = (self.x + 10, self.y + 5, self.width - 20, self.height - 5)  # Defines the accurate hitbox for our character
+        # pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
+        win.blit(pygame.transform.scale(self.img2, (64, 64)), (self.x, self.y))  # scales our image down to 64x64 before drawing
 
-pygame.time.set_timer(USEREVENT + 2, random.randrange(2000, 3500))  # Will trigger every 2 - 3.5 seconds
+class bump(saw):
+    img3 = pygame.image.load(os.path.join('flower.jpg'))
 
+    def draw(self, win):
+        self.hitbox = (self.x + 10, self.y + 5, self.width - 20, self.height - 5)  # Defines the accurate hitbox for our character
+        # pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
+        win.blit(pygame.transform.scale(self.img3, (64, 64)),
+                 (self.x, self.y))  # scales our image down to 64x64 before drawing
 
 class Background:
     pygame.init()
@@ -123,22 +129,22 @@ class Background:
         while menu:
             self.screen.blit(background_image, [0, 0])
             self.screen.blit(character, (character_x, character_y))
-            race = pygame.draw.rect(self.screen, (255, 255, 255), (20, 20, 100, 40))
-            flying = pygame.draw.rect(self.screen, (255, 255, 255), (150, 20, 100, 40))
-            running = pygame.draw.rect(self.screen, (255, 255, 255), (280, 20, 100, 40))
-            help = pygame.draw.rect(self.screen, (207, 185, 151), (420, 20, 50, 40))
+            help = pygame.draw.rect(self.screen, (207, 185, 151), (20, 20, 50, 40))
+            first = pygame.draw.rect(self.screen, (255, 255, 255), (110, 20, 100, 40))
+            second = pygame.draw.rect(self.screen, (255, 255, 255), (250, 20, 100, 40))
+            third = pygame.draw.rect(self.screen, (255, 255, 255), (390, 20, 100, 40))
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     mouse_x = pygame.mouse.get_pos()[0]
                     mouse_y = pygame.mouse.get_pos()[1]
-                    if mouse_x > 20 and mouse_x < 120 and mouse_y > 20 and mouse_y < 60:
-                        self.racetrackbg()
-                    elif mouse_x > 150 and mouse_x < 250 and mouse_y > 20 and mouse_y < 60:
-                        self.flyingbg()
-                    elif mouse_x > 280 and mouse_x < 380 and mouse_y > 20 and mouse_y < 60:
-                        self.runningbg()
-                    elif mouse_x > 420 and mouse_x < 470 and mouse_y > 20 and mouse_y < 60:
+                    if mouse_x > 20 and mouse_x < 70 and mouse_y > 20 and mouse_y < 60:
                         self.helpbg()
+                    elif mouse_x > 110 and mouse_x < 210 and mouse_y > 20 and mouse_y < 60:
+                        self.obstacle1()
+                    elif mouse_x > 250 and mouse_x < 350 and mouse_y > 20 and mouse_y < 60:
+                        self.obstacle2()
+                    elif mouse_x > 390 and mouse_x < 490 and mouse_y > 20 and mouse_y < 60:
+                        self.obstacle3()
 
                 if event.type == pygame.QUIT:
                     menu = False
@@ -167,8 +173,8 @@ class Background:
             pygame.display.flip()
             self.clock.tick(self.clock_rate)
 
-    def racetrackbg(self):
-        pygame.display.set_caption("Race")
+    def obstacle1(self):
+        pygame.display.set_caption("Easy")
         dead = False
 
         self.clock = pygame.time.Clock()
@@ -176,14 +182,22 @@ class Background:
 
         clock = pygame.time.Clock()
 
-        runner = player(0, 220, 64, 64)
+        runner = player(100, 200, 64, 64)
         pygame.time.set_timer(USEREVENT + 1, 500)  # Sets the timer for 0.5 seconds
+        pygame.time.set_timer(USEREVENT + 2, random.randrange(5000, 7000))
 
         run = True
-        speed = 30  # NEW
+        obstacles = []
+        speed = 30
 
-        while run:
+        while run and not runner.falling:
             clock.tick(speed)
+
+            for obstacle in obstacles:
+                obstacle.x -= 1.4
+                if obstacle.x < obstacle.width * -1:  # If our obstacle is off the screen we will remove it
+                    obstacles.pop(obstacles.index(obstacle))
+
             self.bgX -= 1.4  # Move both background images back
             self.bgX2 -= 1.4
 
@@ -198,6 +212,19 @@ class Background:
                     run = False
                     pygame.quit()
                     quit()
+                if event.type == USEREVENT+1:
+                    print("Updating speed")
+                    speed += 1
+                if event.type == USEREVENT + 2:
+                    print("Time for an obstacle")
+                    pygame.time.set_timer(USEREVENT + 2,
+                    random.randrange(5000, 7000))
+
+                    r = 0
+                    if r == 0:
+                        print("log")
+                        log = saw(self.window_width, 200, 64, 64)
+                        obstacles.append(log)
 
             keys = pygame.key.get_pressed()
 
@@ -205,11 +232,12 @@ class Background:
                 if not runner.jumping:  # If we are not already jumping
                     runner.jumping = True
 
-            self.redrawWindow(background_image, runner)
-
+            self.redrawWindow(background_image, runner, obstacles)
+            if runner.falling:
+                self.fallbg()
         menu = True
 
-        while menu:
+        while menu and not runner.falling:
             pygame.time.delay(100)
             self.screen.blit(background_image, [0, 0])
             back = pygame.draw.rect(self.screen, (255, 255, 255), (20, 20, 100, 40))
@@ -223,11 +251,6 @@ class Background:
 
                 if event.type == pygame.QUIT:
                     menu = False
-
-                if event.type == USEREVENT + 2:
-                    r = random.randrange(0, 2)
-                    if r == 0:
-                        obstacles.append(saw(810, 310, 64, 64))
 
             pygame.display.update()
             pygame.display.flip()
@@ -246,24 +269,77 @@ class Background:
             pygame.display.flip()
             self.clock.tick(self.clock_rate)
 
-    def runningbg(self):
-        pygame.display.set_caption("Running")
+    def obstacle2(self):
+        pygame.display.set_caption("Medium")
+
         dead = False
+
         self.clock = pygame.time.Clock()
         background_image = pygame.image.load("haunted.png").convert()
 
-        character = pygame.image.load("duckresized.png")
-        character_x = 0
-        character_y = 220
-        vel = 5
+        clock = pygame.time.Clock()
 
+        runner = player(100, 220, 64, 64)
+        pygame.time.set_timer(USEREVENT + 1, 500)  # Sets the timer for 0.5 seconds
+        pygame.time.set_timer(USEREVENT + 2, random.randrange(3000, 5000))  # Will trigger every 2 - 3.5 seconds
+
+        run = True
+        obstacles = []
+        speed = 30
+
+        while run and not runner.falling:
+            clock.tick(speed)
+
+            for obstacle in obstacles:
+                obstacle.x -= 1.4
+                if obstacle.x < obstacle.width * -1:  # If our obstacle is off the screen we will remove it
+                    obstacles.pop(obstacles.index(obstacle))
+
+            self.bgX -= 1.4  # Move both background images back
+            self.bgX2 -= 1.4
+
+            if self.bgX < self.window_width * -1:  # If our bg is at the -width then reset its position
+                self.bgX = self.window_width
+
+            if self.bgX2 < self.window_width * -1:
+                self.bgX2 = self.window_width
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    pygame.quit()
+                    quit()
+                if event.type == USEREVENT + 1:
+                    print("Updating speed")
+                    speed += 1
+                if event.type == USEREVENT + 2:
+                    print("Time for an obstacle")
+                    pygame.time.set_timer(USEREVENT + 2,
+                    random.randrange(3000, 5000))
+
+                    r = random.randrange(0, 2)
+                    if r == 0:
+                        print("log")
+                        log = saw(self.window_width, 220, 64, 64)
+                        obstacles.append(log)
+                    else:
+                        print("gem")
+                        gem = spike(self.window_width, 220, 64, 64)
+                        obstacles.append(gem)
+            keys = pygame.key.get_pressed()
+
+            if keys[pygame.K_SPACE] or keys[pygame.K_UP]:  # If user hits space or up arrow key
+                if not runner.jumping:  # If we are not already jumping
+                    runner.jumping = True
+
+            self.redrawWindow(background_image, runner, obstacles)
+            if runner.falling:
+                self.fallbg()
         menu = True
 
-        while menu:
+        while menu and not runner.falling:
             pygame.time.delay(100)
             self.screen.blit(background_image, [0, 0])
-            self.screen.blit(character, (character_x, character_y))
-
             back = pygame.draw.rect(self.screen, (255, 255, 255), (20, 20, 100, 40))
 
             for event in pygame.event.get():
@@ -275,13 +351,6 @@ class Background:
 
                 if event.type == pygame.QUIT:
                     menu = False
-
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_LEFT]:
-                character_x -= vel
-
-            if keys[pygame.K_RIGHT]:
-                character_x += vel
 
             pygame.display.update()
             pygame.display.flip()
@@ -300,24 +369,81 @@ class Background:
             pygame.display.flip()
             self.clock.tick(self.clock_rate)
 
-    def flyingbg(self):
-        pygame.display.set_caption("Flying")
+    def obstacle3(self):
+        pygame.display.set_caption("Hard")
+
         dead = False
+
         self.clock = pygame.time.Clock()
         background_image = pygame.image.load("sky2.png").convert()
 
-        character = pygame.image.load("duckresized.png")
-        character_x = 0
-        character_y = 220
-        vel = 5
+        clock = pygame.time.Clock()
 
+        runner = player(100, 220, 64, 64)
+        pygame.time.set_timer(USEREVENT + 1, 500)  # Sets the timer for 0.5 seconds
+        pygame.time.set_timer(USEREVENT + 2, random.randrange(2000, 4000))  # Will trigger every 2 - 3.5 seconds
+
+        run = True
+        obstacles = []
+        speed = 30
+
+        while run and not runner.falling:
+            clock.tick(speed)
+
+            for obstacle in obstacles:
+                obstacle.x -= 1.4
+                if obstacle.x < obstacle.width * -1:  # If our obstacle is off the screen we will remove it
+                    obstacles.pop(obstacles.index(obstacle))
+
+            self.bgX -= 1.4  # Move both background images back
+            self.bgX2 -= 1.4
+
+            if self.bgX < self.window_width * -1:  # If our bg is at the -width then reset its position
+                self.bgX = self.window_width
+
+            if self.bgX2 < self.window_width * -1:
+                self.bgX2 = self.window_width
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    pygame.quit()
+                    quit()
+                if event.type == USEREVENT + 1:
+                    print("Updating speed")
+                    speed += 1
+                if event.type == USEREVENT + 2:
+                    print("Time for an obstacle")
+                    pygame.time.set_timer(USEREVENT + 2,
+                                          random.randrange(2000, 4000))
+
+                    r = random.randrange(0, 2)
+                    if r == 0:
+                        print("log")
+                        log = saw(self.window_width, 200, 64, 64)
+                        obstacles.append(log)
+                    elif r == 0:
+                        print("flower")
+                        flower = bump(self.window_width, 200, 64, 64)
+                        obstacles.append(flower)
+                    else:
+                        print("gem")
+                        gem = spike(self.window_width, 200, 64, 64)
+                        obstacles.append(gem)
+            keys = pygame.key.get_pressed()
+
+            if keys[pygame.K_SPACE] or keys[pygame.K_UP]:  # If user hits space or up arrow key
+                if not runner.jumping:  # If we are not already jumping
+                    runner.jumping = True
+
+            self.redrawWindow(background_image, runner, obstacles)
+            if runner.falling:
+                self.fallbg()
         menu = True
 
-        while menu:
+        while menu and not runner.falling:
             pygame.time.delay(100)
             self.screen.blit(background_image, [0, 0])
-            self.screen.blit(character, (character_x, character_y))
-
             back = pygame.draw.rect(self.screen, (255, 255, 255), (20, 20, 100, 40))
 
             for event in pygame.event.get():
@@ -330,13 +456,6 @@ class Background:
                 if event.type == pygame.QUIT:
                     menu = False
 
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_LEFT]:
-                character_x -= vel
-
-            if keys[pygame.K_RIGHT]:
-                character_x += vel
-
             pygame.display.update()
             pygame.display.flip()
             self.clock.tick(self.clock_rate)
@@ -345,6 +464,43 @@ class Background:
         exit()
 
         while not dead:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    dead = True
+
+            self.screen.blit(background_image, [0, 0])
+
+            pygame.display.flip()
+            self.clock.tick(self.clock_rate)
+
+    def fallbg(self):
+        pygame.display.set_caption("Dead")
+        dead = False
+        self.clock = pygame.time.Clock()
+        background_image = pygame.image.load("diedresized.png").convert()
+
+        menu = True
+
+        while menu:
+            self.screen.blit(background_image, [0, 0])
+            back = pygame.draw.rect(self.screen, (0, 0, 0), (20, 20, 100, 30))
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_x = pygame.mouse.get_pos()[0]
+                    mouse_y = pygame.mouse.get_pos()[1]
+                    if mouse_x > 20 and mouse_x < 120 and mouse_y > 20 and mouse_y < 50:
+                        self.homepagebg()
+
+                if event.type == pygame.QUIT:
+                    menu = False
+                    pygame.quit()
+                    exit()
+
+            pygame.display.update()
+            pygame.display.flip()
+            self.clock.tick(self.clock_rate)
+
+        while (dead == False):
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     dead = True
@@ -391,7 +547,7 @@ class Background:
             pygame.display.flip()
             self.clock.tick(self.clock_rate)
 
-    def redrawWindow(self, bg, runner):
+    def redrawWindow(self, bg, runner, obstacles):
         win.blit(bg, (self.bgX, 0))  # draws our first bg image
         win.blit(bg, (self.bgX2, 0))  # draws the second bg image
         for obstacle in obstacles:
